4062c22a4847b4d339920b8f9e2cc1a165632ae1
Andrew Meyer <ameyer@pivotal.io>
2019-05-17 09:18:55 -0500

    Update for pack 0.2.0
    Signed-off-by: Andrew Meyer <ameyer@pivotal.io>

--- Formula/pack.rb
+++ Formula/pack.rb
@@ -1,8 +1,8 @@
 class Pack < Formula
   desc "A CLI for building apps using Cloud Native Buildpacks"
   homepage "https://github.com/buildpack/pack"
-  url "https://github.com/buildpack/pack/releases/download/v0.1.0/pack-v0.1.0-macos.tgz"
-  sha256 "85565cb51d3c2e346d9b66078a694de8afc796587d258215e506bfcc115a11f3"
+  url "https://github.com/buildpack/pack/releases/download/v0.2.0/pack-v0.2.0-macos.tgz"
+  sha256 "b718b41004a5e21cf76a455e9c109225c531b93d7c2622c83986099c00101634"
 
   bottle :unneeded
 
