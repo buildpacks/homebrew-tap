ef32fc4daf332e4f25c3a8e7dd688ee360ae80c6
Buildpack Robot <cncf-buildpacks-maintainers@lists.cncf.io>
2019-05-20 19:41:02 +0000

    Update formula for pack v0.3.0

--- Formula/pack.rb
+++ Formula/pack.rb
@@ -1,20 +1,8 @@
 class Pack < Formula
   desc "A CLI for building apps using Cloud Native Buildpacks"
   homepage "https://github.com/buildpack/pack"
-  url "https://github.com/buildpack/pack/releases/download/v0.2.0/pack-v0.2.0-macos.tgz"
-  sha256 "b718b41004a5e21cf76a455e9c109225c531b93d7c2622c83986099c00101634"
-
-  bottle :unneeded
-
-  def install
-    bin.install "pack"
-  end
-end
-class Pack < Formula
-  desc "A CLI for building apps using Cloud Native Buildpacks"
-  homepage "https://github.com/buildpack/pack"
   url "https://github.com/buildpack/pack/releases/download/v0.3.0/pack-v0.3.0-macos.tgz"
-  sha256 "f9a26c031ea1965fec74569d51fd2eada23c10a9fb0f2b7cdbea1df11f40b850"
+  sha256 "0ccc6f262d1b37fb7ddc2087535f071f905d66085ca9b6f71722e39bf080fb1a"
 
   bottle :unneeded
 
